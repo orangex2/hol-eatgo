1038e2bbf9e8204a88c9ae2974a505ae2f106b8d
이은학 <ehlee@mz.co.kr>
2020-03-19 19:00:54 +0900

    Add ECR PushLogic

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/WelcomeController.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/WelcomeController.java
@@ -13,6 +13,6 @@ public class WelcomeController {
  */
     @GetMapping("/")
     public String hello() {
-        return "Hello, world!!! " + LocalDateTime.now().toString();
+        return "Hello, world!!!! " + LocalDateTime.now().toString();
     }
 }

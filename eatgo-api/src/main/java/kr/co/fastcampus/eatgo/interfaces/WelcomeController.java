f94c54b77b315a16747cb78b69632c7697fbcc40
이은학 <ehlee@mz.co.kr>
2020-04-06 19:02:10 +0900

    empty

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/WelcomeController.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/WelcomeController.java
@@ -15,8 +15,10 @@ public class WelcomeController {
 
         ZonedDateTime nowSeoul = ZonedDateTime.now(ZoneId.of("Asia/Seoul"));
         System.out.println("System.out.println " + nowSeoul + "-- +9");
-        return "자동배포 : 현재 서울 시각은: " + nowSeoul + "-- +9";
+        return "자동배포(수정2) : 현재 서울 시각은: " + nowSeoul + "-- +9";
     }
 }
+
+
 
 

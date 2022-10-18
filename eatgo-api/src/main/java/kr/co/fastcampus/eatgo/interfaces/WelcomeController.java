16ce4f1b66b8c43883cca28a914d65226dd76520
orangex2 <leehandu@gmail.com>
2022-10-18 07:27:32 +0000

    test-11

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/WelcomeController.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/WelcomeController.java
@@ -11,7 +11,7 @@ public class WelcomeController {
  */
     @GetMapping("/")
     public String hello() {
-	ZonedDateTime nowSeoul = ZonedDateTime.now(ZoneId.of("Asia/Seoul"));
-        return "현재 서울 시각 : "+ nowSeoul + "-- +9";
+	    ZonedDateTime nowSeoul = ZonedDateTime.now(ZoneId.of("Asia/Seoul"));
+	    return "현재 서울 시각 : " + nowSeoul + "-- +9";
     }
 }
